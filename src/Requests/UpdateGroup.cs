07e36f820203905eebce27f4bb1ffdec3bd1c64c
Eryckson Magno <eryckson@albedo.team>
2021-02-22 14:49:43 +0000

    Merged PR 88: displayname added

--- src/Requests/UpdateGroup.cs
+++ src/Requests/UpdateGroup.cs
@@ -5,6 +5,7 @@
         string AccountId { get; set; }
         string Id { get; set; }
         string Name { get; set; }
+        string DisplayName { get; set; }
         string Description { get; set; }
         bool IsDefault { get; set; }
     }
