0fc9d442762ce327974ee93e84d0c8c2e02efdfb
Eryckson Magno <eryckson@albedo.team>
2021-03-22 23:05:21 +0000

    Merged PR 242: get pwd recovery by token

---
+++ src/Responses/PasswordRecoveryResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AlbedoTeam.Identity.Contracts.Responses
+{
+    public interface PasswordRecoveryResponse
+    {
+        string Id { get; set; }
+        string AccountId { get; set; }
+        string UserId { get; set; }
+        string ValidationToken { get; set; }
+        DateTime ExpiresAt { get; set; }
+
+        DateTime CreatedAt { get; set; }
+        DateTime? UpdatedAt { get; set; }
+        bool IsDeleted { get; set; }
+        DateTime? DeletedAt { get; set; }
+    }
+}
